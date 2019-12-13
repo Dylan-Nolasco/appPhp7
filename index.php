396548d398443aa9d3e42c4684f818e5c21430cb
Dylan-Nolasco <58802195+Dylan-Nolasco@users.noreply.github.com>
2019-12-13 13:43:48 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,62 +1,65 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html lang="es-mx">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Science Technology Engineerig & Mathematics</title>
-    <link rel="stylesheet" href="">
-    <link rel="stylesheet" href="css/estilos.css">
+    <title>IntroPhp7 Grupo XA</title>
+    <link rel="stylesheet" href="css/estilos.css"/>
     <link rel="stylesheet" href="css/menu.css"/>
     <link rel="stylesheet" href="css/problema.css"/>
     <link href="https://fonts.googleapis.com/css?family=Rambla" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
-    <script>
-        function solucion(){
-        var velocidadAngular = 71;
-        var radio = 0.8;;
-        var velocidadLineal = 71 * 0.8;
-        var d=document.getElementById("resultado");
-        d.innerHTML='La velocidad lineal o tangencial de la partícula es de : '+ velocidadLineal +' m/s.';
-        }
-    </script>
 </head>
 <body>
-    <section id="contenedor">
-    <header id="gridHeader">
-    <h1>Science Technology Engineerig & Mathematics</h1>
-    <h2>Resolver problemas de Ciencia e Ingeniería con JS</h2>
-    <h2>Nombre: Dylan Elías Nolasco López No. de control: 17091119</h2>
-    <img src="fotoSelfie.jpg" alt="su foto">
+   <section class="wrapper">
+    <header>
+      <h1 class="logo"><a href="index.php">IntroPHP7</a></h1>
+      <nav>
+      <ul>
+        <li><a href="index.php" class="current">Inicio</a></li>
+        <li><a href="#">Otros Ejemplos Php7</a></li>
+      </ul>
+      </nav>
     </header>
-    </section>
-    <section id="problema"  class="gridProblema">
-      <section id=descripcion>
-            <h2>Problema: Cálcular la velocidad lineal de cuerpos.</h2>
+<section id="contenedor">
+<section  class="problema">
+ <h1>Problema: Cálcular la velocidad lineal de cuerpos.</h1>
             <p>Descripción:</p>
             <p>Dterminar la velocidad lineal o tangencial de una partícula que tiene una velocidad angular cuya magnitud es de 71 rad/s y su radio de giro es de 0.8 mts <br>
-      </section>
-      <section id="datos">
-            <h2>Datos:</h2>
-            radio = r = 0.8 mts <br>
-            velocidad angular = w = 71 rad/s. 
-      </section>
-      <section id="formulas">
-            <h2>Fórmulas</h2>
-            w = θ/t donde w = velocidad angular, θ = desplazamiento angular, t = tiempo <br>
-            Vt = velocidad lineal = w * r
-      </section>
-      <section id="solucion">
-            <h2>Solución</h2>
-            <p>La velocidad lineal es:<br>
-             Vt = w * r entonces sustituyendo los datos (71 rad/s) * (0.8 mts)</p>
-             <button onclick="solucion()">Presiona para calcular</button>
-      </section>
-      
-      <section id="resultado"></section>
-    </section>
-    <footer id="gridPie">
-        <p>Solution Science Problems. 2019 Creative Commons 3.0</p>
+</section>
+<section class="formulas">
+<h2>Fórmulas</h2>
+    w = θ/t donde w = velocidad angular, θ = desplazamiento angular, t = tiempo <br>
+    Vt = velocidad lineal = w * r
+</section>
+<section class="datos">
+<h2>Datos:</h2>
+   radio = r = 0.8 mts <br>
+    velocidad angular = w = 71 rad/s. 
+</section>
+<section class="calculos">
+<h2>Solución</h2>
+   <p>La velocidad lineal es:<br>
+    Vt = w * r entonces sustituyendo los datos (71 rad/s) * (0.8 mts)</p>
+</section>
+<?php
+     function calcula_densidad(){
+        $velocidadAngular = 71;
+        $radio = 0.8;
+        $velocidadLineal = 71 * 0.8;       
+        return $velocidadLineal;
+     }
+?>
+<section class="resultado">
+<h2>Resultado:</h2>
+<div id="resultadoA"></div>
+<?php
+ print "<h1> resultado: densidad = ".calcula_densidad(). "m/s</h1>";
+?>
+</section>
+</section>
+    <footer class="pie">
+     Creative Commons versión 3.0 SciSoft 2019
     </footer>
+   </section>
 </body>
 </html>
